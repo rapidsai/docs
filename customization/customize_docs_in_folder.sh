0a9930c176ab7afa1cc438b9e157a4b33119bb7b
AJ Schmidt <aschmidt@nvidia.com>
2020-03-12 10:01:19 -0400

    migrate customization scripts

---
+++ customization/customize_docs_in_folder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+FOLDER_TO_CUSTOMIZE=$1
+
+if [ ! -d "${FOLDER_TO_CUSTOMIZE}" ]; then
+  echo "Couldn't find subfolder: ${FOLDER_TO_CUSTOMIZE}"
+  exit 1
+fi
+
+SCRIPT_SRC_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # directory of where this script is located
+SPHINX_SEARCH_TERM='class="wy-side-nav-search"'
+DOXYGEN_SEARCH_TERM='id="titlearea"'
+CURRENT_STABLE_VERSION="12"
+
+# IFS is changed due to cuxfilter docs having spaces in their filenames
+OIFS="$IFS"
+IFS=$'\n'
+for FILE in $(grep "${SPHINX_SEARCH_TERM}\|${DOXYGEN_SEARCH_TERM}" -rl \
+  --include=\*.html \
+  --exclude-dir=stable \
+  --exclude-dir=nightly \
+  --exclude-dir=latest \
+  --exclude-dir=legacy \
+  ${FOLDER_TO_CUSTOMIZE} ); do
+  python ${SCRIPT_SRC_FOLDER}/customize_doc.py $(realpath ${FILE}) ${CURRENT_STABLE_VERSION}
+  echo "" # line break for readability
+done
+IFS="$OIFS"
---
+++ customization/lib_map.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# This script generates a lib_map.json file that maps each RAPIDS library
+# to its default documentation page for each version (legacy, stable, nightly).
+# i.e.
+# { "libcudf": {
+#     "stable": "/libcudf/stable/namespacecudf.html",
+#     "nightly": "/libcudf/nightly/namespacecudf.html",
+#     "legacy": "/libcudf/legacy/namespacecudf.html"
+#   }
+#   "cuspatial": {
+#     "stable": null,
+#     "nightly": "/cuspatial/en/nightly/api.html",
+#     "legacy": null
+#    }, ...}
+
+
+SCRIPT_SRC_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # directory where this script is located
+PROJ_ROOT=$(PWD)
+FOLDER_MAP="{}"
+
+for FOLDER in api/*/ ; do
+
+  LIB=$(basename ${FOLDER}) # remove api/ & trailing slash from folder
+  DEFAULT_PATH=${LIB}
+  cd ${FOLDER}
+
+  FOLDER_MAP=$(echo ${FOLDER_MAP} | jq ". + {${LIB}: {}}") # initialize empty JSON object for lib
+
+  LIB_ROOT=${DEFAULT_PATH}
+  for VERSION in "nightly" "stable" "legacy"; do
+    DEFAULT_PATH=${LIB_ROOT}
+    FOLDER_MAP=$(echo ${FOLDER_MAP} | jq ".${LIB}.${VERSION} = null")
+
+    if [ -d "${VERSION}" ]; then
+      DEFAULT_PATH+="/${VERSION}"
+
+      if [[ "${LIB}" =~ ^(rmm|libnvstrings)$ ]]; then
+        DEFAULT_PATH+="/annotated.html"
+
+      elif [ "${LIB}" = libcudf ]; then
+        DEFAULT_PATH+="/namespacecudf.html"
+
+      elif [ -f "${VERSION}/api.html" ]; then
+        DEFAULT_PATH+="/api.html"
+      fi
+
+      FOLDER_MAP=$(echo ${FOLDER_MAP} | jq ".${LIB}.${VERSION} = \"/${DEFAULT_PATH}\"") # Add version entry to lib object
+    fi
+  done
+
+  cd ${PROJ_ROOT}
+done
+
+
+echo ${FOLDER_MAP} | jq '.' > ${SCRIPT_SRC_FOLDER}/lib_map.json
