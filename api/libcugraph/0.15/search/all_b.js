06d7d177bcc7320d281fa5c30bb26b5a9b8b3444
gpuCI <38199262+GPUtester@users.noreply.github.com>
2020-08-07 10:03:19 +0000

    [gpuCI] Update docs for cugraph v0.15

--- api/libcugraph/0.15/search/all_b.js
+++ api/libcugraph/0.15/search/all_b.js
@@ -4,5 +4,5 @@ var searchData=
   ['loadbalanceexecution_80',['LoadBalanceExecution',['../classcugraph_1_1mg_1_1detail_1_1LoadBalanceExecution.html',1,'cugraph::mg::detail']]],
   ['logdistribution_81',['LogDistribution',['../classcugraph_1_1mg_1_1detail_1_1LogDistribution.html',1,'cugraph::mg::detail']]],
   ['logic_5ferror_82',['logic_error',['../structcugraph_1_1logic__error.html',1,'cugraph']]],
-  ['louvain_83',['louvain',['../namespacecugraph.html#ad6e6ce5956268ce5f1133930a3cee5bb',1,'cugraph']]]
+  ['louvain_83',['louvain',['../namespacecugraph.html#abaf7d59f72c36f359b30f201fe78583f',1,'cugraph']]]
 ];
