37285ec8af92fc4643c02abc47b73d1a1d0a0b74
gpuCI <38199262+GPUtester@users.noreply.github.com>
2020-08-12 09:33:31 +0000

    [gpuCI] Update docs for cudf v0.15

--- api/cudf/0.15/_static/documentation_options.js
+++ api/cudf/0.15/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.ga1142fe',
+    VERSION: '0+untagged.1.g1f6eb0a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
