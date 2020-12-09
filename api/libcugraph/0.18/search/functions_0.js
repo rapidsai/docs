a4c29a1b5ef7dcc9492672f7f1b24eb8be6e3372
gpuCI <38199262+GPUtester@users.noreply.github.com>
2020-12-09 20:57:18 +0000

    [gpuCI] Update docs for cugraph v0.18

---
+++ api/libcugraph/0.18/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['betweenness_5fcentrality_354',['betweenness_centrality',['../namespacecugraph.html#afa8eadf92af49adf18fe1403b7c5d62e',1,'cugraph']]],
+  ['bfs_355',['bfs',['../namespacecugraph.html#a007a963984961523802b2b66a6dd33db',1,'cugraph']]]
+];
