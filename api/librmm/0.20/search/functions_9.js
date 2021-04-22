8e7268328cc80090e5e1707dd997e1f61ccd9289
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-04-22 19:46:31 +0000

    [gpuCI] Update docs for rmm v0.20

---
+++ api/librmm/0.20/search/functions_9.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['limiting_5fresource_5fadaptor_288',['limiting_resource_adaptor',['../classrmm_1_1mr_1_1limiting__resource__adaptor.html#ace34d73d5251628ef769f3666ce8fe0b',1,'rmm::mr::limiting_resource_adaptor']]],
+  ['log_5foutstanding_5fallocations_289',['log_outstanding_allocations',['../classrmm_1_1mr_1_1tracking__resource__adaptor.html#adae81547e8ea0e045b786d24b3162656',1,'rmm::mr::tracking_resource_adaptor']]],
+  ['logging_5fresource_5fadaptor_290',['logging_resource_adaptor',['../classrmm_1_1mr_1_1logging__resource__adaptor.html#a67c7dfb43ee4d2fbf35c8b44d8bbd40e',1,'rmm::mr::logging_resource_adaptor::logging_resource_adaptor(Upstream *upstream, std::string const &amp;filename=get_default_filename(), bool auto_flush=false)'],['../classrmm_1_1mr_1_1logging__resource__adaptor.html#a9e4d1ec70c8a7a873721772b75abd00a',1,'rmm::mr::logging_resource_adaptor::logging_resource_adaptor(Upstream *upstream, std::ostream &amp;stream, bool auto_flush=false)']]]
+];
---
+++ api/librmm/0.20/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['allocated_5fpointer_339',['allocated_pointer',['../structrmm_1_1mr_1_1detail_1_1stream__ordered__memory__resource_1_1split__block.html#ac0b35f2cf9733b50535f047ad9242920',1,'rmm::mr::detail::stream_ordered_memory_resource::split_block']]]
+];
