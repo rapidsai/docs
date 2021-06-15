eea1402e7ed7f0f24f9a004e00ecf62eb043bfb0
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-06-15 18:07:56 +0000

    [gpuCI] Update docs for rmm v21.08

---
+++ api/librmm/21.08/search/classes_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['pinned_5fmemory_5fresource_210',['pinned_memory_resource',['../classrmm_1_1mr_1_1pinned__memory__resource.html',1,'rmm::mr']]],
+  ['polymorphic_5fallocator_211',['polymorphic_allocator',['../classrmm_1_1mr_1_1polymorphic__allocator.html',1,'rmm::mr']]],
+  ['pool_5fmemory_5fresource_212',['pool_memory_resource',['../classrmm_1_1mr_1_1pool__memory__resource.html',1,'rmm::mr']]]
+];
---
+++ api/librmm/21.08/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['exec_5fpolicy_2ehpp_226',['exec_policy.hpp',['../exec__policy_8hpp.html',1,'']]]
+];
