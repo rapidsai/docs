9126c11e3b792c53b1d5764034ec311e6e6a4d5e
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-06-20 10:53:44 +0000

    [gpuCI] Update docs for cugraph v21.08

--- api/libcugraph/21.08/search/functions_7.js
+++ api/libcugraph/21.08/search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hungarian_492',['hungarian',['../namespacecugraph.html#aca205fb3baf295e7fb95857b998aab7c',1,'cugraph::hungarian(raft::handle_t const &amp;handle, GraphCOOView&lt; vertex_t, edge_t, weight_t &gt; const &amp;graph, vertex_t num_workers, vertex_t const *workers, vertex_t *assignment)'],['../namespacecugraph.html#adac8693d6e776a3cf078b48de9ae841e',1,'cugraph::hungarian(raft::handle_t const &amp;handle, GraphCOOView&lt; vertex_t, edge_t, weight_t &gt; const &amp;graph, vertex_t num_workers, vertex_t const *workers, vertex_t *assignment, weight_t precision)']]]
+  ['hungarian_492',['hungarian',['../namespacecugraph.html#a6709c56fbbb108b4d54b5f3d8b542bdf',1,'cugraph::hungarian(raft::handle_t const &amp;handle, GraphCOOView&lt; vertex_t, edge_t, weight_t &gt; const &amp;graph, vertex_t num_workers, vertex_t const *workers, vertex_t *assignments)'],['../namespacecugraph.html#aeaae3689f14f2c3a41e59ce326b08116',1,'cugraph::hungarian(raft::handle_t const &amp;handle, GraphCOOView&lt; vertex_t, edge_t, weight_t &gt; const &amp;graph, vertex_t num_workers, vertex_t const *workers, vertex_t *assignments, weight_t epsilon)']]]
 ];
