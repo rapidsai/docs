6089a21acffe9eced810c60ddb8f0223e7eeb93a
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-09-19 09:49:17 +0000

    [gpuCI] Update docs for cudf v21.10

--- api/libcudf/21.10/search/functions_18.js
+++ api/libcudf/21.10/search/functions_18.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['_7ebinning_5fmemory_5fresource_3020',['~binning_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1binning__memory__resource.html#a0ff3bef0cfd9f5a5bf8e70fc3369b307',1,'rmm::mr::binning_memory_resource']]],
+  ['_7ebinning_5fmemory_5fresource_3020',['~binning_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1binning__memory__resource.html#a66b18a38aeb16944ed9da36c51691b4e',1,'rmm::mr::binning_memory_resource']]],
   ['_7ebuffer_3021',['~buffer',['../classcudf_1_1io_1_1datasource_1_1buffer.html#a733c8294af2a75c527c14e942be39935',1,'cudf::io::datasource::buffer']]],
   ['_7edata_5fsink_3022',['~data_sink',['../classcudf_1_1io_1_1data__sink.html#a778f6bad53ffc49dc6285dd8a9879f73',1,'cudf::io::data_sink']]],
   ['_7edatasource_3023',['~datasource',['../classcudf_1_1io_1_1datasource.html#aa4486e8cfc15e495a15030f70d362437',1,'cudf::io::datasource']]],
   ['_7edevice_5fbuffer_3024',['~device_buffer',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1device__buffer.html#a361c327d720bb5916e7b09748d8806fd',1,'rmm::device_buffer']]],
-  ['_7efixed_5fsize_5fmemory_5fresource_3025',['~fixed_size_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1fixed__size__memory__resource.html#aa0c748d2c233130b3c54fb3ee7c80e19',1,'rmm::mr::fixed_size_memory_resource']]],
+  ['_7efixed_5fsize_5fmemory_5fresource_3025',['~fixed_size_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1fixed__size__memory__resource.html#a64ea84c68c1c208adffae85a7827fac9',1,'rmm::mr::fixed_size_memory_resource']]],
   ['_7eglobal_5farena_3026',['~global_arena',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1detail_1_1arena_1_1global__arena.html#a16bbded32f0ed473296d5ad10dfc0b64',1,'rmm::mr::detail::arena::global_arena']]],
-  ['_7epool_5fmemory_5fresource_3027',['~pool_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1pool__memory__resource.html#afcc226c485d432831a431edeffc58753',1,'rmm::mr::pool_memory_resource']]]
+  ['_7epool_5fmemory_5fresource_3027',['~pool_memory_resource',['https://docs.rapids.ai/api/librmm/21.10/classrmm_1_1mr_1_1pool__memory__resource.html#acf2d916010f1bdb218f214f09821b3bf',1,'rmm::mr::pool_memory_resource']]]
 ];
