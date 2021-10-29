706a20c9185f1a6e99fa3bb24d7633109eeb3de2
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-10-29 10:17:30 +0000

    [gpuCI] Update docs for cugraph v21.12

--- api/libcugraph/21.12/search/classes_e.js
+++ api/libcugraph/21.12/search/classes_e.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['partition_5ft_576',['partition_t',['../classcugraph_1_1partition__t.html',1,'cugraph']]],
   ['permutation_5ffunctor_577',['permutation_functor',['../structcugraph_1_1detail_1_1permutation__functor.html',1,'cugraph::detail']]],
-  ['property_5fadd_578',['property_add',['../structcugraph_1_1property__add.html',1,'cugraph']]],
-  ['property_5fadd_3c_20thrust_3a_3atuple_3c_20args_2e_2e_2e_20_3e_20_3e_579',['property_add&lt; thrust::tuple&lt; Args... &gt; &gt;',['../structcugraph_1_1property__add_3_01thrust_1_1tuple_3_01Args_8_8_8_01_4_01_4.html',1,'cugraph']]]
+  ['property_5fop_578',['property_op',['../structcugraph_1_1property__op.html',1,'cugraph']]],
+  ['property_5fop_3c_20thrust_3a_3atuple_3c_20args_2e_2e_2e_20_3e_2c_20op_20_3e_579',['property_op&lt; thrust::tuple&lt; Args... &gt;, Op &gt;',['../structcugraph_1_1property__op_3_01thrust_1_1tuple_3_01Args_8_8_8_01_4_00_01Op_01_4.html',1,'cugraph']]]
 ];
