f30fb041897a0f6b1e36ba479b4c974883dd300a
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-12-15 22:32:42 +0000

    [gpuCI] Update docs for rmm v22.02

---
+++ api/librmm/22.02/search/classes_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['bad_5falloc_175',['bad_alloc',['../classrmm_1_1bad__alloc.html',1,'rmm']]],
+  ['binning_5fmemory_5fresource_176',['binning_memory_resource',['../classrmm_1_1mr_1_1binning__memory__resource.html',1,'rmm::mr']]],
+  ['block_177',['block',['../classrmm_1_1mr_1_1detail_1_1arena_1_1block.html',1,'rmm::mr::detail::arena::block'],['../structrmm_1_1mr_1_1detail_1_1block.html',1,'rmm::mr::detail::block']]],
+  ['block_5fbase_178',['block_base',['../structrmm_1_1mr_1_1detail_1_1block__base.html',1,'rmm::mr::detail']]],
+  ['bytes_179',['bytes',['../structrmm_1_1detail_1_1bytes.html',1,'rmm::detail']]]
+];
