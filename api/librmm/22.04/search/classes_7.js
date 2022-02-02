e409413b80e28e0397ba7bd8bb50c05c6152e340
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-02-02 21:01:04 +0000

    [gpuCI] Update docs for rmm v22.04

---
+++ api/librmm/22.04/search/classes_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['host_5fmemory_5fresource_217',['host_memory_resource',['../classrmm_1_1mr_1_1host__memory__resource.html',1,'rmm::mr']]]
+];
---
+++ api/librmm/22.04/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['per_5fdevice_5fresource_2ehpp_244',['per_device_resource.hpp',['../per__device__resource_8hpp.html',1,'']]]
+];
