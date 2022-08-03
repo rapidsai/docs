5f117d7c8af8e79620a9302f1c71f32bab8d0057
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-08-03 11:25:03 +0000

    [gpuCI] Update docs for kvikio v22.08

--- api/libkvikio/22.08/search/functions_4.js
+++ api/libkvikio/22.08/search/functions_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['parallelize_5floop_60',['parallelize_loop',['../classkvikio_1_1third__party_1_1thread__pool.html#af087386271a3cca7446880d9f9223bbd',1,'kvikio::third_party::thread_pool']]],
-  ['pread_61',['pread',['../classkvikio_1_1FileHandle.html#a5b6e1d51645b48700c59083e0af099cc',1,'kvikio::FileHandle']]],
+  ['pread_61',['pread',['../classkvikio_1_1FileHandle.html#ab254c5290909fcbf0347d4815ae512c4',1,'kvikio::FileHandle']]],
   ['push_5ftask_62',['push_task',['../classkvikio_1_1third__party_1_1thread__pool.html#a9762205a5edd34b0e55f0eab713fb309',1,'kvikio::third_party::thread_pool::push_task(const F &amp;task)'],['../classkvikio_1_1third__party_1_1thread__pool.html#a4b43f7fe72465af23d3bc16080125eb2',1,'kvikio::third_party::thread_pool::push_task(const F &amp;task, const A &amp;... args)']]],
-  ['pwrite_63',['pwrite',['../classkvikio_1_1FileHandle.html#abeb82ee92dd7c0f439701721da6f610a',1,'kvikio::FileHandle']]]
+  ['pwrite_63',['pwrite',['../classkvikio_1_1FileHandle.html#ad7ba5fdf03baa5fe16cbd8d2fd7a98d7',1,'kvikio::FileHandle']]]
 ];
