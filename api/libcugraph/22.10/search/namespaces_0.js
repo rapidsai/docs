162dfc70427476d40961ccd5dd8ba2d7a348f26d
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-09-05 13:52:42 +0000

    [gpuCI] Update docs for cugraph v22.10

--- api/libcugraph/22.10/search/namespaces_0.js
+++ api/libcugraph/22.10/search/namespaces_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cugraph_846',['cugraph',['../namespacecugraph.html',1,'']]],
-  ['ext_5fraft_847',['ext_raft',['../namespacecugraph_1_1ext__raft.html',1,'cugraph']]]
+  ['cugraph_845',['cugraph',['../namespacecugraph.html',1,'']]],
+  ['ext_5fraft_846',['ext_raft',['../namespacecugraph_1_1ext__raft.html',1,'cugraph']]]
 ];
