3ed29c8d5d98eea6d1510fb8aea3035d3ac61ea0
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-10-07 11:28:31 +0000

    [gpuCI] Update docs for dask-cuda v22.10

--- api/dask-cuda/22.10/_static/documentation_options.js
+++ api/dask-cuda/22.10/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.10.00a0+gd3972fd',
+    VERSION: '22.10.00a0+g60ea5be',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
