7e81b9fa00206fe5414120abbc260b3fac9c1ffd
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-10-16 09:00:20 +0000

    [gpuCI] Update docs for rmm v22.12

---
+++ api/librmm/22.12/search/enumvalues_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['posix_5ffile_5fdescriptor_406',['posix_file_descriptor',['../classrmm_1_1mr_1_1cuda__async__memory__resource.html#a1c7b108a007542d70bc7078cf0307b85a9edf15eedc33844b49f362208f03662d',1,'rmm::mr::cuda_async_memory_resource']]]
+];
---
+++ api/librmm/22.12/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghilmnoprstuvw~",
+  1: "abcdefghlmnoprst",
+  2: "dep",
+  3: "abcdefghilmoprstuvw~",
+  4: "dmp",
+  5: "es",
+  6: "a",
+  7: "npw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator"
+};
+
