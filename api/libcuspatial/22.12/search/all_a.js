2ec316ada66f6ce7ac67576ca2d0d9f394f31f21
gpuCI <38199262+GPUtester@users.noreply.github.com>
2022-11-09 12:47:25 +0000

    [gpuCI] Update docs for cuspatial v22.12

--- api/libcuspatial/22.12/search/all_a.js
+++ api/libcuspatial/22.12/search/all_a.js
@@ -1,10 +1,12 @@
 var searchData=
 [
   ['libcuspatial_20c_2b_2b_20developer_20guide_48',['libcuspatial C++ Developer Guide',['../DEVELOPER_GUIDE.html',1,'']]],
-  ['linestring_5fdistance_2ecuh_49',['linestring_distance.cuh',['../linestring__distance_8cuh.html',1,'']]],
-  ['linestring_5fdistance_2ehpp_50',['linestring_distance.hpp',['../linestring__distance_8hpp.html',1,'']]],
-  ['linestring_5fref_51',['linestring_ref',['../classcuspatial_1_1linestring__ref.html',1,'cuspatial']]],
-  ['logic_5ferror_52',['logic_error',['../structcuspatial_1_1logic__error.html',1,'cuspatial']]],
-  ['lonlat_5fto_5fcartesian_53',['lonlat_to_cartesian',['../group__coordinate__transform.html#ga68569f920b8fca1c3ff77450ba883afb',1,'cuspatial::lonlat_to_cartesian(double origin_lon, double origin_lat, cudf::column_view const &amp;input_lon, cudf::column_view const &amp;input_lat, rmm::mr::device_memory_resource *mr=rmm::mr::get_current_device_resource())'],['../coordinate__transform_8cuh.html#ab9acbb25a944c9180252721057f66dc6',1,'cuspatial::lonlat_to_cartesian(InputIt lon_lat_first, InputIt lon_lat_last, OutputIt xy_first, vec_2d&lt; T &gt; origin, rmm::cuda_stream_view stream=rmm::cuda_stream_default)']]],
-  ['libcuspatial_20c_2b_2b_20documentation_20guide_54',['libcuspatial C++ Documentation Guide',['../md_developer_guide_DOCUMENTATION.html',1,'']]]
+  ['linestring_5fbounding_5fbox_2ehpp_49',['linestring_bounding_box.hpp',['../linestring__bounding__box_8hpp.html',1,'']]],
+  ['linestring_5fbounding_5fboxes_50',['linestring_bounding_boxes',['../group__spatial__relationship.html#ga99c52b855579395f6df48bd6601e2276',1,'cuspatial']]],
+  ['linestring_5fdistance_2ecuh_51',['linestring_distance.cuh',['../linestring__distance_8cuh.html',1,'']]],
+  ['linestring_5fdistance_2ehpp_52',['linestring_distance.hpp',['../linestring__distance_8hpp.html',1,'']]],
+  ['linestring_5fref_53',['linestring_ref',['../classcuspatial_1_1linestring__ref.html',1,'cuspatial']]],
+  ['logic_5ferror_54',['logic_error',['../structcuspatial_1_1logic__error.html',1,'cuspatial']]],
+  ['lonlat_5fto_5fcartesian_55',['lonlat_to_cartesian',['../group__coordinate__transform.html#ga68569f920b8fca1c3ff77450ba883afb',1,'cuspatial::lonlat_to_cartesian(double origin_lon, double origin_lat, cudf::column_view const &amp;input_lon, cudf::column_view const &amp;input_lat, rmm::mr::device_memory_resource *mr=rmm::mr::get_current_device_resource())'],['../coordinate__transform_8cuh.html#ab9acbb25a944c9180252721057f66dc6',1,'cuspatial::lonlat_to_cartesian(InputIt lon_lat_first, InputIt lon_lat_last, OutputIt xy_first, vec_2d&lt; T &gt; origin, rmm::cuda_stream_view stream=rmm::cuda_stream_default)']]],
+  ['libcuspatial_20c_2b_2b_20documentation_20guide_56',['libcuspatial C++ Documentation Guide',['../md_developer_guide_DOCUMENTATION.html',1,'']]]
 ];
--- api/libcuspatial/22.12/search/all_c.js
+++ api/libcuspatial/22.12/search/all_c.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['nearest_5flinestring_5fgeometry_5fid_69',['nearest_linestring_geometry_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#aff0cd6fec0cf7c35a1059875788a80b5',1,'cuspatial::point_linestring_nearest_points_result']]],
-  ['nearest_5fpoint_5fgeometry_5fid_70',['nearest_point_geometry_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a8f47489732c2dba07b272857c078f138',1,'cuspatial::point_linestring_nearest_points_result']]],
-  ['nearest_5fpoint_5fon_5flinestring_5fxy_71',['nearest_point_on_linestring_xy',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a476b33b65ba577e6d2d1e7ae55566c69',1,'cuspatial::point_linestring_nearest_points_result']]],
-  ['nearest_20points_72',['Nearest Points',['../group__nearest__points.html',1,'']]],
-  ['nearest_5fsegment_5fid_73',['nearest_segment_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a309d4af9e6047c1947e5c3fa6217767f',1,'cuspatial::point_linestring_nearest_points_result']]],
-  ['num_5flinestrings_74',['num_linestrings',['../classcuspatial_1_1multilinestring__ref.html#ac9b114232004ab4d77b6ef5a40c3d19c',1,'cuspatial::multilinestring_ref::num_linestrings()'],['../classcuspatial_1_1multilinestring__range.html#aeef29db55d873bc6cce772abda888868',1,'cuspatial::multilinestring_range::num_linestrings()']]],
-  ['num_5fmultilinestrings_75',['num_multilinestrings',['../classcuspatial_1_1multilinestring__range.html#a73569c1cbe538473c89011e671c53c44',1,'cuspatial::multilinestring_range']]],
-  ['num_5fpoints_76',['num_points',['../classcuspatial_1_1multilinestring__range.html#ae7b6f60b6d9d5b72fa1c1d73463b2479',1,'cuspatial::multilinestring_range']]]
+  ['nearest_5flinestring_5fgeometry_5fid_71',['nearest_linestring_geometry_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#aff0cd6fec0cf7c35a1059875788a80b5',1,'cuspatial::point_linestring_nearest_points_result']]],
+  ['nearest_5fpoint_5fgeometry_5fid_72',['nearest_point_geometry_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a8f47489732c2dba07b272857c078f138',1,'cuspatial::point_linestring_nearest_points_result']]],
+  ['nearest_5fpoint_5fon_5flinestring_5fxy_73',['nearest_point_on_linestring_xy',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a476b33b65ba577e6d2d1e7ae55566c69',1,'cuspatial::point_linestring_nearest_points_result']]],
+  ['nearest_20points_74',['Nearest Points',['../group__nearest__points.html',1,'']]],
+  ['nearest_5fsegment_5fid_75',['nearest_segment_id',['../structcuspatial_1_1point__linestring__nearest__points__result.html#a309d4af9e6047c1947e5c3fa6217767f',1,'cuspatial::point_linestring_nearest_points_result']]],
+  ['num_5flinestrings_76',['num_linestrings',['../classcuspatial_1_1multilinestring__ref.html#ac9b114232004ab4d77b6ef5a40c3d19c',1,'cuspatial::multilinestring_ref::num_linestrings()'],['../classcuspatial_1_1multilinestring__range.html#aeef29db55d873bc6cce772abda888868',1,'cuspatial::multilinestring_range::num_linestrings()']]],
+  ['num_5fmultilinestrings_77',['num_multilinestrings',['../classcuspatial_1_1multilinestring__range.html#a73569c1cbe538473c89011e671c53c44',1,'cuspatial::multilinestring_range']]],
+  ['num_5fpoints_78',['num_points',['../classcuspatial_1_1multilinestring__range.html#ae7b6f60b6d9d5b72fa1c1d73463b2479',1,'cuspatial::multilinestring_range']]]
 ];
