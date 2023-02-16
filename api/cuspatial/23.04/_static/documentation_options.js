449c262d5faef2c5339463bf664f6f06053097da
gpuCI <38199262+GPUtester@users.noreply.github.com>
2023-02-16 12:07:41 +0000

    [gpuCI] Update docs for cuspatial v23.04

--- api/cuspatial/23.04/_static/documentation_options.js
+++ api/cuspatial/23.04/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '23.04.00',
-    LANGUAGE: 'en-US',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
