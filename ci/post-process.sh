fd75a8c2e555bcf4f8d5287f46aae4f11c493ec1
AJ Schmidt <aschmidt@nvidia.com>
2023-04-19 09:48:25 -0400

    rm TODO. post-process all docs

--- ci/post-process.sh
+++ ci/post-process.sh
@@ -9,6 +9,4 @@ pip install -r "${CURRENT_DIR}/customization/requirements.txt"
 
 "${CURRENT_DIR}"/customization/lib_map.sh
 
-# TODO: change "_site/api/rmm" to "_site/api" after everything is confirmed
-# working
-"${CURRENT_DIR}"/customization/customize_docs_in_folder.sh "_site/api/rmm"
+"${CURRENT_DIR}"/customization/customize_docs_in_folder.sh "_site/api"
