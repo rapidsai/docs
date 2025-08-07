bfe229366bdf9d6b7208b0c982a7efab285054b4
James Lamb <jlamb@nvidia.com>
2025-08-07 15:28:00 -0500

    fix post-processing for inactive projects

--- ci/update_symlinks.sh
+++ ci/update_symlinks.sh
@@ -8,51 +8,54 @@
 #######################################
 set -euEo pipefail
 
+# expect paths to be relative to the project root
 PROJ_ROOT=$(realpath "$(dirname $(realpath $0))/../")
-RELEASES="${PROJ_ROOT}/_data/releases.json"
+pushd "${PROJ_ROOT}"
 
-STABLE_VERSION=$(jq -r '.stable.version' < "${RELEASES}")
-LEGACY_VERSION=$(jq -r '.legacy.version' < "${RELEASES}")
-NIGHTLY_VERSION=$(jq -r '.nightly.version' < "${RELEASES}")
-
-STABLE_UCXX_VERSION=$(jq -r '.stable.ucxx_version' < "${RELEASES}")
-LEGACY_UCXX_VERSION=$(jq -r '.legacy.ucxx_version' < "${RELEASES}")
-NIGHTLY_UCXX_VERSION=$(jq -r '.nightly.ucxx_version' < "${RELEASES}")
+PROJECTS_TO_VERSIONS_JSON=$(./ci/get-projects-to-versions.sh)
 
 echo "Updating symlinks..."
 echo ""
-for FOLDER in _site/api/*/ ; do
-  if [[ "${FOLDER}" == *"ucxx"* ]]; then
-    STABLE_FOLDER=$STABLE_UCXX_VERSION
-    LEGACY_FOLDER=$LEGACY_UCXX_VERSION
-    NIGHTLY_FOLDER=$NIGHTLY_UCXX_VERSION
-  else
-    STABLE_FOLDER=$STABLE_VERSION
-    LEGACY_FOLDER=$LEGACY_VERSION
-    NIGHTLY_FOLDER=$NIGHTLY_VERSION
-  fi
+for PROJECT in $(jq -r 'keys | .[]' <<< "${PROJECTS_TO_VERSIONS_JSON}"); do
+  VERSIONS_FOR_THIS_PROJECT=$(
+    jq \
+      -r \
+      --arg pr "${PROJECT}" \
+      '.[$pr]' \
+    <<< "${PROJECTS_TO_VERSIONS_JSON}"
+  )
 
-  cd ${FOLDER}
+  # expect to find a local folder, relative to the root of the repo,
+  # named e.g. '_site/api/cudf'
+  FOLDER="_site/api/${PROJECT}/"
+  pushd "${FOLDER}"
   echo ""
   echo "${FOLDER}--------"
 
-  if [ -d "${STABLE_FOLDER}" ]; then
-    ln -s ${STABLE_FOLDER} stable
-    ln -s ${STABLE_FOLDER} latest
-    echo "  - stable & latest point to ${STABLE_FOLDER}"
-  fi
+  # loop over 'stable', 'nightly', etc.
+  for VERSION_NAME in $(jq -r 'keys | .[]' <<< "${VERSIONS_FOR_THIS_PROJECT}"); do
+    # expect to find a directory matching the version number, e.g. '_site/api/cudf/25.10'
+    VERSION_NUMBER=$(
+      jq \
+        -r \
+        --arg version_name "${VERSION_NAME}" \
+        '.[$version_name]' \
+      <<< "${VERSIONS_FOR_THIS_PROJECT}"
+    )
+    FOLDER_FOR_THIS_VERSION="${VERSION_NUMBER}"
 
-  if [ -d "${LEGACY_FOLDER}" ]; then
-    ln -s ${LEGACY_FOLDER} legacy
-    echo "  - legacy points to ${LEGACY_FOLDER}"
-  fi
-
-  if [ -d "${NIGHTLY_FOLDER}" ]; then
-    ln -s ${NIGHTLY_FOLDER} nightly
-    echo "  - nightly points to ${NIGHTLY_FOLDER}"
-  fi
+    # map /latest to the same version as /stable
+    if [[ "${VERSION_NAME}" == "stable" ]]; then
+      ls -s "${FOLDER_FOR_THIS_VERSION}" stable
+      ls -s "${FOLDER_FOR_THIS_VERSION}" latest
+      echo "  - 'stable' and 'latest' point to '${FOLDER_FOR_THIS_VERSION}'"
+    else
+      ls -s "${FOLDER_FOR_THIS_VERSION}" "${VERSION_NAME}"
+      echo "  - '${VERSION_NAME}' points to '${FOLDER_FOR_THIS_VERSION}'"
+    fi
+  done # for VERSION
 
   echo "---------------"
   echo ""
-  cd ${PROJ_ROOT}
-done
+  popd
+done # for PROJECT
